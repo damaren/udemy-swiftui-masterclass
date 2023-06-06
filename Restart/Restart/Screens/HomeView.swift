b2ed3a7af743fb6ee4bc80ae44e5c7232d7bd1cb
José Damaren <josedamaren@Joses-Mac-mini.local>
2023-06-06 17:25:18 -0300

    Finished implementation of the 'Restart' app.

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -11,20 +11,66 @@ struct HomeView: View {
     // MARK: - PROPERTY
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
     
     // MARK: - BODY
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.2)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(
+                        Animation
+                            .easeInOut(duration: 4)
+                            .repeatForever()
+                        , value: isAnimating)
+            }
+            
+            // MARK: - CENTER
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            
+            Spacer()
             
             Button(action: {
-                isOnboardingViewActive = true
+                withAnimation {
+                    playSound(sound: "success", type: "m4a")
+                    isOnboardingViewActive = true
+                }
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            } //: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         } //: VSTACK
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
     }
 }
 
